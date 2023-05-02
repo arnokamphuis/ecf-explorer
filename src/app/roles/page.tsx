9aaf64e4f2e600f2ac8030c880b1deab6ed5d305
Floris <florisjoepie@gmail.com>
2023-05-02 23:50:28 +0200

    refactor: getcomplete roles for readability

--- src/app/roles/page.tsx
+++ src/app/roles/page.tsx
@@ -1,70 +1,60 @@
-import { Roles } from "@/types/role";
-import { Levels } from "@/types/level";
 import Slider from "@/components/slider";
-import path from "path";
-import { promises as fs } from "fs";
-import { Competencies } from "@/types/competency";
 import RoleFilter from "@/components/roleFilter";
 import RoleSelect from "@/components/roleselect";
+import { getLevels } from "@/utils/getLevels";
+import { getRoles } from "@/utils/getRoles";
+import { getCompetences } from "@/utils/getCompetences";
+import { cache } from "react";
+import { LevelRoleDescription } from "@/types/level";
 
-const jsonDirectory = path.join(process.cwd(), "json");
-async function getLevels() {
-	const levels: Levels = JSON.parse(
-		await fs.readFile(jsonDirectory + "/levels.json", "utf8")
-	);
-	return levels;
-}
-async function getRoles(): Promise<Roles> {
-	const fileContents = await fs.readFile(jsonDirectory + "/roles.json", "utf8");
-
-	return JSON.parse(fileContents);
-}
-
-async function getCompleteRoles() {
+const getCompleteRoles = cache(async () => {
 	const levels = await getLevels();
-	const competencies: Competencies = JSON.parse(
-		await fs.readFile(jsonDirectory + "/competencies.json", "utf8")
-	);
-	return Object.keys(levels).map(level => ({
-		developed: levels[level]["developed"].map(role => ({
-			...role,
-			developed: role.developed.map(competence => ({
-				name: `${competence} (${competencies[competence].phase})`,
-				activity: competencies[competence].levels[`${Number(level) - 1}`],
-			})),
-			"in development": role["in development"].map(competence => ({
-				name: `${competence} (${competencies[competence].phase})`,
-				activity: competencies[competence].levels[level],
-			})),
-		})),
-		"in development": levels[level]["in development"].map(role => ({
-			...role,
-			developed: role.developed.map(competence => ({
-				name: `${competence} (${competencies[competence].phase})`,
-				activity: competencies[competence].levels[`${Number(level) - 1}`],
-			})),
-			"in development": role["in development"].map(competence => ({
-				name: `${competence} (${competencies[competence].phase})`,
-				activity: competencies[competence].levels[level],
-			})),
-		})),
-	}));
-}
+	const competences = await getCompetences();
 
-async function getRolesPerLevel() {
+	const mapCompetence = (
+		competence: string,
+		level: string,
+		developed = false
+	) => ({
+		name: `${competence} (${competences[competence].phase})`,
+		activity:
+			competences[competence].levels[developed ? Number(level) - 1 : level],
+	});
+
+	const mapRole = (role: LevelRoleDescription, level: string) => ({
+		...role,
+		developed: role.developed.map(comp => mapCompetence(comp, level, true)),
+		"in development": role["in development"].map(comp =>
+			mapCompetence(comp, level)
+		),
+	});
+
+	return Object.keys(levels).map(level => ({
+		developed: levels[level]["developed"].map(role => mapRole(role, level)),
+		"in development": levels[level]["in development"].map(role =>
+			mapRole(role, level)
+		),
+	}));
+});
+
+const getRolesPerLevel = cache(async () => {
 	const levels = await getLevels();
 
 	return Object.keys(levels).map(level => [
 		...levels[level].developed.map(role => role.name),
 		...levels[level]["in development"].map(role => role.name),
 	]);
-}
+});
 
 export default async function Page() {
-	const roleData = await getRoles();
-	const roles = Object.keys(roleData);
-	const completeRoles = await getCompleteRoles();
-	const rolesPerLevel = await getRolesPerLevel();
+	const [completeRoles, rolesPerLevel, roles] = await Promise.all([
+		getCompleteRoles(),
+		getRolesPerLevel(),
+		getRoles(),
+	]);
+
+	const roleNames = Object.keys(roles);
+
 	return (
 		<div>
 			<p className="font-bold text-lg mb-10">Level</p>
@@ -72,7 +62,7 @@ export default async function Page() {
 
 			<div className="flex flex-col flex-wrap bg-[#121212] p-4 rounded-lg gap-4 mt-5">
 				<p>Selecteer een of meerdere rollen</p>
-				<RoleSelect roleNames={roles} />
+				<RoleSelect roleNames={roleNames} />
 			</div>
 			<RoleFilter allRoles={completeRoles} rolesPerLevel={rolesPerLevel} />
 		</div>
