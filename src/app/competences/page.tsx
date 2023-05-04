c74e0effd500f4a0b89e6f568ea9d13dd6cf66f0
Floris <florisjoepie@gmail.com>
2023-05-04 14:14:58 +0200

    feat: added phase sections to competence overview

--- src/app/competences/page.tsx
+++ src/app/competences/page.tsx
@@ -1,14 +1,44 @@
 import Link from "next/link";
 import { getCompetences } from "@/utils/getCompetences";
 
+type Section = {
+	title: string;
+	data: string[];
+};
+
 export default async function Competencespage() {
 	const competences = await getCompetences();
+
+	const phaseSections: Section[] = [];
+	Object.keys(competences).forEach(comp => {
+		const existingSection = phaseSections.find(
+			section => section.title === competences[comp].phase
+		);
+		if (existingSection) {
+			existingSection.data.push(comp);
+		} else {
+			phaseSections.push({
+				title: competences[comp].phase,
+				data: [comp],
+			});
+		}
+	});
+
 	return (
 		<div className="flex flex-col flex-wrap h-[80vh]">
-			{Object.keys(competences).map(comp => (
-				<Link href={`/competences/${comp.replaceAll(" ", "-")}`} key={comp}>
-					{comp}
-				</Link>
+			{phaseSections.map(section => (
+				<div key={section.title}>
+					<p>{section.title}</p>
+					<div className="flex flex-col">
+						{section.data.map(comp => (
+							<Link
+								href={`/competences/${comp.replaceAll(" ", "-")}`}
+								key={comp}>
+								{comp}
+							</Link>
+						))}
+					</div>
+				</div>
 			))}
 		</div>
 	);
